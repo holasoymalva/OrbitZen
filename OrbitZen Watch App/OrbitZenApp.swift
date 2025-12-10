841edab477a72489c48b0e26c725f4aa167c2d3e
holasoymalva <alquimista3301@gmail.com>
2025-12-10 11:22:38 -0600

    Initial Commit

---
+++ OrbitZen Watch App/OrbitZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitZenApp.swift
+//  OrbitZen Watch App
+//
+//  Created by malva on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OrbitZen_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrbitZen Watch AppTests/OrbitZen_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitZen_Watch_AppTests.swift
+//  OrbitZen Watch AppTests
+//
+//  Created by malva on 10/12/25.
+//
+
+import Testing
+@testable import OrbitZen_Watch_App
+
+struct OrbitZen_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
